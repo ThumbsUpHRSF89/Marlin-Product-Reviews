c0918389fed6a0f21778799c62bb3cfb9cc8522a
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-02-21 19:56:18 -0800

    data update

--- sampleData.js
+++ sampleData.js
@@ -4,18 +4,16 @@ const tvData = [
     name: 'Sony-X',
     category: 'TV',
     image_url: 'https://goo.gl/images/WnZqdY',
-    rating: 2,
-    numberOfReviews: 758,
-    tvImageUrl: 'https://goo.gl/images/WnZqdY' 
+    rating: 0,
+    numberOfReviews: 254,
   },
   {
     id: 76,
     name: 'Vizio-B',
     category: 'TV',
     image_url: 'https://goo.gl/images/aw6khe',
-    rating: 4,
-    numberOfReviews: 695,
-    tvImageUrl: 'https://goo.gl/images/aw6khe' 
+    rating: 3,
+    numberOfReviews: 573,
   },
   {
     id: 77,
@@ -23,17 +21,15 @@ const tvData = [
     category: 'TV',
     image_url: 'https://goo.gl/images/BFRLPi',
     rating: 5,
-    numberOfReviews: 104,
-    tvImageUrl: 'https://goo.gl/images/BFRLPi' 
+    numberOfReviews: 733,
   },
   {
     id: 78,
     name: 'Samsung-M',
     category: 'TV',
     image_url: 'https://goo.gl/images/t4HW6Q',
-    rating: 5,
-    numberOfReviews: 15,
-    tvImageUrl: 'https://goo.gl/images/t4HW6Q' 
+    rating: 0,
+    numberOfReviews: 373,
   },
   {
     id: 79,
@@ -41,71 +37,63 @@ const tvData = [
     category: 'TV',
     image_url: 'https://goo.gl/images/6wpBKq',
     rating: 3,
-    numberOfReviews: 755,
-    tvImageUrl: 'https://goo.gl/images/6wpBKq' 
+    numberOfReviews: 125,
   },
   {
     id: 80,
     name: 'LG-X',
     category: 'TV',
     image_url: 'https://goo.gl/images/myL1tv',
-    rating: 5,
-    numberOfReviews: 619,
-    tvImageUrl: 'https://goo.gl/images/myL1tv' 
+    rating: 3,
+    numberOfReviews: 98,
   },
   {
     id: 81,
     name: 'undefined-B',
     category: 'TV',
     image_url: 'https://goo.gl/images/7NmS2A',
-    rating: 2,
-    numberOfReviews: 723,
-    tvImageUrl: 'https://goo.gl/images/7NmS2A' 
+    rating: 0,
+    numberOfReviews: 714,
   },
   {
     id: 82,
     name: 'Sony-V',
     category: 'TV',
     image_url: 'https://goo.gl/images/CNzaV1',
-    rating: 3,
-    numberOfReviews: 492,
-    tvImageUrl: 'https://goo.gl/images/CNzaV1' 
+    rating: 1,
+    numberOfReviews: 338,
   },
   {
     id: 83,
     name: 'Vizio-M',
     category: 'TV',
     image_url: 'https://goo.gl/images/ypwmt1',
-    rating: 5,
-    numberOfReviews: 264,
-    tvImageUrl: 'https://goo.gl/images/ypwmt1' 
+    rating: 3,
+    numberOfReviews: 100,
   },
   {
     id: 84,
     name: 'Philips-undefined',
     category: 'TV',
     image_url: 'https://goo.gl/images/RoVfZb',
-    rating: 4,
-    numberOfReviews: 249,
-    tvImageUrl: 'https://goo.gl/images/RoVfZb' 
+    rating: 3,
+    numberOfReviews: 700,
   },
   {
     id: 85,
     name: 'Samsung-X',
     category: 'TV',
     image_url: 'https://goo.gl/images/qJgKz5',
-    rating: 4,
-    numberOfReviews: 727,
-    tvImageUrl: 'https://goo.gl/images/qJgKz5' 
+    rating: 1,
+    numberOfReviews: 375,
   },
   {
     id: 86,
     name: 'TCL-B',
     category: 'TV',
     image_url: 'https://goo.gl/images/u68PL7',
-    rating: 1,
-    numberOfReviews: 868,
-    tvImageUrl: 'https://goo.gl/images/u68PL7' 
+    rating: 4,
+    numberOfReviews: 594,
   },
   {
     id: 87,
@@ -113,118 +101,105 @@ const tvData = [
     category: 'TV',
     image_url: 'https://goo.gl/images/er6emf',
     rating: 1,
-    numberOfReviews: 283,
-    tvImageUrl: 'https://goo.gl/images/er6emf' 
+    numberOfReviews: 317,
   },
   {
     id: 88,
     name: 'undefined-M',
     category: 'TV',
     image_url: 'https://goo.gl/images/7Rq9BV',
-    rating: 3,
-    numberOfReviews: 81,
-    tvImageUrl: 'https://goo.gl/images/7Rq9BV' 
+    rating: 2,
+    numberOfReviews: 498,
   },
   {
     id: 89,
     name: 'Sony-undefined',
     category: 'TV',
     image_url: 'https://goo.gl/images/ZGqiKk',
-    rating: 5,
-    numberOfReviews: 366,
-    tvImageUrl: 'https://goo.gl/images/ZGqiKk' 
+    rating: 1,
+    numberOfReviews: 474,
   },
   {
     id: 90,
     name: 'Vizio-X',
     category: 'TV',
     image_url: 'https://goo.gl/images/qHFMdA',
-    rating: 2,
-    numberOfReviews: 133,
-    tvImageUrl: 'https://goo.gl/images/qHFMdA' 
+    rating: 1,
+    numberOfReviews: 398,
   },
   {
     id: 91,
     name: 'Philips-B',
     category: 'TV',
     image_url: 'https://goo.gl/images/u9Q8DR',
-    rating: 1,
-    numberOfReviews: 672,
-    tvImageUrl: 'https://goo.gl/images/u9Q8DR' 
+    rating: 3,
+    numberOfReviews: 765,
   },
   {
     id: 92,
     name: 'Samsung-V',
     category: 'TV',
     image_url: 'https://goo.gl/images/aVSsYn',
-    rating: 2,
-    numberOfReviews: 725,
-    tvImageUrl: 'https://goo.gl/images/aVSsYn' 
+    rating: 5,
+    numberOfReviews: 291,
   },
   {
     id: 93,
     name: 'TCL-M',
     category: 'TV',
     image_url: 'https://goo.gl/images/oj4pSr',
-    rating: 4,
-    numberOfReviews: 243,
-    tvImageUrl: 'https://goo.gl/images/oj4pSr' 
+    rating: 5,
+    numberOfReviews: 861,
   },
   {
     id: 94,
     name: 'LG-undefined',
     category: 'TV',
     image_url: 'https://goo.gl/images/SSJw9D',
-    rating: 2,
-    numberOfReviews: 334,
-    tvImageUrl: 'https://goo.gl/images/SSJw9D' 
+    rating: 3,
+    numberOfReviews: 323,
   },
   {
     id: 95,
     name: 'undefined-X',
     category: 'TV',
     image_url: 'https://goo.gl/images/88vHa9',
-    rating: 2,
-    numberOfReviews: 270,
-    tvImageUrl: 'https://goo.gl/images/88vHa9' 
+    rating: 1,
+    numberOfReviews: 510,
   },
   {
     id: 96,
     name: 'Sony-B',
     category: 'TV',
     image_url: 'https://goo.gl/images/JoWhVh',
-    rating: 5,
-    numberOfReviews: 37,
-    tvImageUrl: 'https://goo.gl/images/JoWhVh' 
+    rating: 0,
+    numberOfReviews: 885,
   },
   {
     id: 97,
     name: 'Vizio-V',
     category: 'TV',
     image_url: 'https://goo.gl/images/18VjJX',
-    rating: 0,
-    numberOfReviews: 865,
-    tvImageUrl: 'https://goo.gl/images/18VjJX' 
+    rating: 4,
+    numberOfReviews: 452,
   },
   {
     id: 98,
     name: 'Philips-M',
     category: 'TV',
     image_url: 'https://goo.gl/images/fVmpkb',
-    rating: 3,
-    numberOfReviews: 116,
-    tvImageUrl: 'https://goo.gl/images/fVmpkb' 
+    rating: 5,
+    numberOfReviews: 719,
   },
   {
     id: 99,
     name: 'Samsung-undefined',
     category: 'TV',
     image_url: 'https://goo.gl/images/RtURx5',
-    rating: 0,
-    numberOfReviews: 100,
-    tvImageUrl: 'https://goo.gl/images/RtURx5' 
+    rating: 2,
+    numberOfReviews: 962,
   },
-]
+];
 
 const allProductData = [];
 
@@ -250,12 +225,11 @@ const repeat = () => {
     const obj = {
       // plus 75 for product numbers 75 - 99
       id: i + 75,
-      name: tvBrands[i % 7] + '-' + letters[i % 5],
+      name: `${tvBrands[i % 7]}-${letters[i % 5]}`,
       category: 'TV',
-      image_url: tvImageUrls[i],
+      image_url: tvImageUrls[i % 26],
       rating: getRandomInt(0, 5),
       numberOfReviews: getRandomInt(0, 1000),
-      tvImageUrl: tvImageUrls[i % 26],
     };
     objects.push(obj);
   }
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './client/src/customerReviews.jsx',
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.join(__dirname, '/client/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './client/dist',
+  },
+};
