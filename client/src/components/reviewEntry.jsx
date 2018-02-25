ade1e20ac32fe3c5363770fed16485ae2853c546
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-02-25 14:27:13 -0800

    adds styles and formats review entries

--- client/src/components/reviewEntry.jsx
+++ client/src/components/reviewEntry.jsx
@@ -3,12 +3,23 @@ import React from 'react';
 const ReviewEntry = (props) => {
   return (
     <div className="reviewEntry">
-      <div>Profile Photo</div>
-      <div>{props.review.username}</div>
-      <div>{props.review.rating}</div>
-      <h5>{props.review.header}</h5>
-      <div>{props.review.date}</div>
+      <div className="profileBanner">
+        <img className="profileImage" src="/images/profile1.png" alt="profile" />
+        <span className="username">{props.review.username}</span>
+      </div>
+      <div className="starHeader">
+        <div className="stars">{props.review.rating}</div>
+        <h5 className="header">{props.review.header}</h5>
+      </div>
+      <div className="date">{props.review.date}</div>
+      <div className="verified">Verified Purchase</div>
       <p>{props.review.text}</p>
+      <div>
+        <span className="commentLink">Comment</span>
+        <span className="useful">Was this review helpful to you?</span>
+        <button className="yesNo">Yes</button>
+        <button className="yesNo">No</button>
+      </div>
     </div>
   );
 };
