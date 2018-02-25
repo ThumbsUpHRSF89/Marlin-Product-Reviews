96e9d9e5b2375f57838fbb43241c24db5c933d4e
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-02-25 11:59:32 -0800

    Serves static html from any url request, and receives product id query from component

--- server/server.js
+++ server/server.js
@@ -2,16 +2,31 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const reviewController = require('../db/models/reviews.js');
 
 const app = express();
 const port = 8002;
 
-mongoose.connect('mongodb://localhost/products');
+mongoose.connect('mongodb://localhost/hackazon');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../client/dist/')));
 
+app.get('/product/*', (req, res) => {
+  const productID = req.query.id;
+  reviewController.findByProductID(productID, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Gotcha at port ${port}!\nLove,\nYour Server`);
 });
-
