c2becfc6fad438d514ff0b7549885fd6682e6844
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-02-25 22:00:34 -0800

    set up testing components

---
+++ client/src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ReviewSection from './reviewSection';
+
+ReactDOM.render(<ReviewSection />, document.getElementById('ReviewSection'));
