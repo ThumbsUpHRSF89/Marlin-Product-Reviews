47ebca291d940457dc3781da4b1620bee144c6b2
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-02-26 19:53:54 -0800

    adds destructuring from props

--- client/src/components/reviewEntry.jsx
+++ client/src/components/reviewEntry.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 
 const ReviewEntry = (props) => {
+
+  const {
+    product_id,
+    username,
+    text,
+    rating,
+    header,
+    date,
+  } = props.review;
+
+
   return (
     <div className="reviewEntry">
       <div className="profileBanner">
         <img className="profileImage" src="/images/profile1.png" alt="profile" />
-        <span className="username">{props.review.username}</span>
+        <span className="username">{username}</span>
       </div>
       <div className="starHeader">
-        <div className="stars">{props.review.rating}</div>
-        <h5 className="header">{props.review.header}</h5>
+        <div className="stars">{rating}</div>
+        <h5 className="header">{header}</h5>
       </div>
-      <div className="date">{props.review.date}</div>
+      <div className="date">{date}</div>
       <div className="verified">Verified Purchase</div>
-      <p>{props.review.text}</p>
+      <p>{text}</p>
       <div>
         <span className="commentLink">Comment</span>
         <span className="useful">Was this review helpful to you?</span>
--- client/src/components/reviewList.jsx
+++ client/src/components/reviewList.jsx
@@ -3,11 +3,12 @@ import ReviewEntry from './reviewEntry';
 
 const ReviewList = (props) => {
   console.log('props', props);
+  const { reviews } = props;
 
   return (
     <div>
       <h4>Top Customer Reviews</h4>
-      {props.reviews.map((review) => <ReviewEntry review={review} key={review._id} />)}
+      {reviews.map((review) => <ReviewEntry review={review} key={review._id} />)}
     </div>
   );
 };
