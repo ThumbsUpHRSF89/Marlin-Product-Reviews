ac4013b653bcca375069867d35dfc9eae0307b10
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-02-27 19:17:25 -0800

    Adds testing for saving and retrieving correct data

--- test/db.test.js
+++ test/db.test.js
@@ -3,8 +3,35 @@ const Reviews = require('../db/models/reviews.js');
 
 mongoose.connect('mongodb://localhost/hackazonReviews');
 
-describe('Tests for database', () => {
-  it('should get review data and return it in an array', () => {
+describe('Database Testing', () => {
+  it('should save a review model and find it in db with id', () => {
+    const data = {
+      product_id: 999,
+      username: 'sara',
+      text: "this was ok.  did the job, but wasn't as great as they made it sound",
+      rating: 0,
+      header: 'Started off strong....aaaaaaand failed :( *UPDATES',
+      date: 'August 14, 2018',
+    };
+
+    Reviews.saveAll(data, (err) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log('yaaay data stored succesfully');
+      }
+    });
+
+    Reviews.findByProductID(999, (err, results) => {
+      if (err) {
+        throw err;
+      } else {
+        expect(results.username).toBe(data.username);
+      }
+    });
+  });
+
+  it('should get review data and return it in an array of objects', () => {
     Reviews.findByProductID(1, (err, results) => {
       if (err) {
         throw err;
@@ -15,11 +42,12 @@ describe('Tests for database', () => {
     });
   });
 
-  it('should return an array of objects with the correct properties', () => {
+  it('should return an array of objects with the correct keys', () => {
     Reviews.findByProductID(1, (err, results) => {
       if (err) {
         throw err;
       } else {
+        expect(typeof results[0]).toBe('object');
         expect(results[0]).toHaveProperty('product_id', 1);
         expect(results[0]).toHaveProperty('product_id');
         expect(results[0]).toHaveProperty('username');
@@ -27,6 +55,21 @@ describe('Tests for database', () => {
         expect(results[0]).toHaveProperty('rating');
         expect(results[0]).toHaveProperty('header');
         expect(results[0]).toHaveProperty('date');
+      }
+    });
+  });
+
+  it('should return data in correct format', () => {
+    Reviews.findByProductID(1, (err, results) => {
+      if (err) {
+        throw err;
+      } else {
+        expect(typeof results[0].product_id).toBe('number');
+        expect(typeof results[0].username).toBe('string');
+        expect(typeof results[0].text).toBe('string');
+        expect(typeof results[0].rating).toBe('number');
+        expect(typeof results[0].header).toBe('string');
+        expect(typeof results[0].date).toBe('string');
       }
     });
   });
