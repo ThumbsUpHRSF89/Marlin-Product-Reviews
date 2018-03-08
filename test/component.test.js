44b53f02bcca48b6a0bc57dde5afe66ef635015c
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-03-08 12:08:24 -0800

    adds superficial testing

--- test/component.test.js
+++ test/component.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DummyData from '../test/dummyData';
 import ReviewSection from '../client/src/reviewSection';
 import CustomerImages from '../client/src/components/customerImages';
 import Histogram from '../client/src/components/histogram';
@@ -7,7 +8,6 @@ import PageButtons from '../client/src/components/pageButtons';
 import RecentReviews from '../client/src/components/recentReviews';
 import ReviewEntry from '../client/src/components/reviewEntry';
 import ReviewList from '../client/src/components/reviewList';
-
 
 describe('<ReviewSection />', () => {
   it('renders 1 <ReviewSection /> component', () => {
@@ -25,7 +25,7 @@ describe('<CustomerImages />', () => {
 
 describe('<Histogram />', () => {
   it('renders 1 <Histogram /> component', () => {
-    const component = shallow(<Histogram />);
+    const component = shallow(<Histogram reviews={DummyData} />);
     expect(component).toHaveLength(1);
   });
 });
@@ -44,9 +44,23 @@ describe('<PageButtons />', () => {
   });
 });
 
+describe('<RecentReviews />', () => {
+  it('renders 1 <RecentReviews /> component', () => {
+    const component = shallow(<RecentReviews />);
+    expect(component).toHaveLength(1);
+  });
+});
+
 describe('<ReviewList />', () => {
   it('renders 1 <ReviewList /> component', () => {
-    const component = shallow(<ReviewList />);
+    const component = shallow(<ReviewList reviews={DummyData} />);
+    expect(component).toHaveLength(1);
+  });
+});
+
+describe('<ReviewEntry />', () => {
+  it('renders 1 <ReviewEntry /> component', () => {
+    const component = shallow(<ReviewEntry review={DummyData[0]} />);
     expect(component).toHaveLength(1);
   });
 });
